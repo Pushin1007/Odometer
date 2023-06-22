b770c28503314b94f82c4ab289d1eab2502706c8
Pushin <geiby@mail.ru>
2023-06-22 18:07:16 +0300

    добавлено проверка разрешений, и всплывающее уведомление для запроса разрешения

--- app/src/main/java/com/example/odometer/MainActivity.java
+++ app/src/main/java/com/example/odometer/MainActivity.java
@@ -1,11 +1,17 @@
 package com.example.odometer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.content.ContextCompat;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
@@ -16,15 +22,26 @@ import java.util.Locale;
 public class MainActivity extends AppCompatActivity {
     private OdometerService odometer;//Используется для OdometerService.
     private boolean bound = false;//Хранит информацию о том, связана активность со службой или нет.
+    private final int PERMISSION_REQUEST_CODE = 698;// значение используемое для запроса разрешения
+    private final int NOTIFICATION_ID = 423;//Константа для идентификатора уведомления.
 
     @Override
     protected void onStart() {
         super.onStart();
-        Intent intent = new Intent(this, OdometerService.class); //Интент, отправленный OdometerService.
-        // связывание службы производится при запуске активности
-        bindService(intent, connection,//Объект ServiceConnection.
-                Context.BIND_AUTO_CREATE); //Метод bindService() использует интент и соединение со службой для связывания активности со службой.
-        //флаг Context.BIND_AUTO_CREATE приказывает Android создать службу, если она еще не существует.
+        if (ContextCompat.checkSelfPermission(this, OdometerService.PERMISSION_STRING)
+                //если разрешение небыло предоставлено
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,//запросить его во время выполнения
+                    new String[]{OdometerService.PERMISSION_STRING},
+                    PERMISSION_REQUEST_CODE);
+        } else {
+//если разрешение уже было выдано, выполнить связывание со службой
+            Intent intent = new Intent(this, OdometerService.class); //Интент, отправленный OdometerService.
+            // связывание службы производится при запуске активности
+            bindService(intent, connection,//Объект ServiceConnection.
+                    Context.BIND_AUTO_CREATE); //Метод bindService() использует интент и соединение со службой для связывания активности со службой.
+            //флаг Context.BIND_AUTO_CREATE приказывает Android создать службу, если она еще не существует.
+        }
     }
 
     private ServiceConnection connection = new ServiceConnection() {//Необходимо определить объект ServiceConnection.
@@ -77,5 +94,48 @@ public class MainActivity extends AppCompatActivity {
                 handler.postDelayed(this, 1000); // значение обновляется каждую секунду
             }
         });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        //Метод onRequestPermissionsResult() возвращает результаты ваших запросов разрешений.
+        //super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case PERMISSION_REQUEST_CODE: { //Проверить, совпадает ли код с тем, который был использован в методе requestPermissions().
+                //Если запрос был отменен, результаты не возвращаются.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Intent intent = new Intent(this, OdometerService.class);
+                    bindService(intent, connection, Context.BIND_AUTO_CREATE);
+                } else {
+
+                    NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
+                            //Эти настройки необходимы для всех уведомлений.
+                            .setSmallIcon(android.R.drawable.ic_menu_compass)
+                            .setContentTitle(getResources().getString(R.string.app_name))
+                            .setContentText(getResources().getString(R.string.permission_denied))
+                            //А эти — только для всплывающих уведомлений.
+                            .setPriority(NotificationCompat.PRIORITY_HIGH)
+                            .setVibrate(new long[]{1000, 1000})
+                            //уведомление исчезает плсле щелчка
+                            .setAutoCancel(true);
+                    //создание действия
+                    Intent actionIntent = new Intent(this, MainActivity.class);
+                    //Добавление PendingIntent к уведомлению означает, что уведомление будет запускать MainActivity по щелчку.
+                    PendingIntent actionPendingIntent = PendingIntent.getActivity(
+                            this,
+                            0,
+                            actionIntent,
+                            PendingIntent.FLAG_UPDATE_CURRENT);
+                    builder.setContentIntent(actionPendingIntent);
+//Выдача уведомления
+                    NotificationManager notificationManager =
+                            (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+                    notificationManager.notify(NOTIFICATION_ID, builder.build());
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/example/odometer/OdometerService.java
+++ app/src/main/java/com/example/odometer/OdometerService.java
@@ -1,16 +1,78 @@
 package com.example.odometer;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Binder;
+import android.os.Bundle;
 import android.os.IBinder;
 
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
+
+import java.util.List;
 import java.util.Random;
 
 
 public class OdometerService extends Service {
+    private LocationListener listener;//слушатель позиционирования, приватный чтобы был доступен извне
     private final IBinder binder = new OdometerBinder();
     private final Random random = new Random();// временно возвращаем случайное число
+
+    private static double distanceInMeters;// расстояние
+
+    private static Location lastLocation = null; // последнее местоположение
+
+    private LocationManager locManager;// Для объекта LocationManager создается приватная переменная, чтобы к нему можно было обращаться из других методов.
+    public static final String PERMISSION_STRING
+            = android.Manifest.permission.ACCESS_FINE_LOCATION; //Строка разрешения добавляется в виде константы.
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        listener = new LocationListener() {// создаем слушателя
+            @Override
+            public void onLocationChanged(@NonNull Location location) { //Location описывает текущее местоположение
+                if (lastLocation == null) {
+                    lastLocation = location; //Задает исходное местонахождение пользователя.
+                }
+                distanceInMeters += location.distanceTo(lastLocation); //Обновляет пройденное расстояние и последнее местонахождение пользователя.
+                lastLocation = location;
+                //distanceInMeters =location.getSpeed();// можно показывать скорость
+            }
+
+            @Override
+            public void onFlushComplete(int requestCode) {
+                LocationListener.super.onFlushComplete(requestCode);
+            }
+
+            @Override
+            public void onProviderDisabled(String arg0) {
+            }
+
+            @Override
+            public void onProviderEnabled(String arg0) {
+            }
+
+            @Override
+            public void onStatusChanged(String arg0, int arg1, Bundle bundle) {
+            }
+        };
+        locManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE); //Получить объект LocationManager.
+        if (ContextCompat.checkSelfPermission(this, PERMISSION_STRING) // проверить наличие разрешения
+                == PackageManager.PERMISSION_GRANTED) {
+            String provider = locManager.getBestProvider(new Criteria(), true); //Получить самого точного  провайдера местоположения
+            if (provider != null) {
+                locManager.requestLocationUpdates(provider, 1000, 1, listener);
+            }
+        }
+
+    }
 
     public class OdometerBinder extends Binder {
         OdometerService getOdometer() { //Метод используется активностью для получения ссылки на OdometerService.
@@ -24,6 +86,22 @@ public class OdometerService extends Service {
     }
 
     public double getDistance() {//получаем случайное вещественное число
-        return random.nextDouble();
+        //return random.nextDouble();
+        // Расстояние в метрах преобразуется в мили. При желании точность вычислений можно было бы повысить, но для наших целей хватит и этой точности.
+        return this.distanceInMeters / 1609.344;
+
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (locManager != null && listener != null) {
+            if (ContextCompat.checkSelfPermission(this, PERMISSION_STRING)
+                    == PackageManager.PERMISSION_GRANTED) {
+                locManager.removeUpdates(listener); //Прекратить получение обновлений (если имеется разрешение  на их удаление).
+            }
+            locManager = null;
+            listener = null;
+        }
     }
 }
